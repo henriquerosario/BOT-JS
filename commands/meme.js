a54c3e535e99db4c606e7fdb5aa3a6a2518417c6
Henrique Franchesco <henriquefranchesco50@gmail.com>
2021-02-04 02:39:59 +0000

    update no filtro e gif no comando meme

--- commands/meme.js
+++ commands/meme.js
@@ -2,18 +2,9 @@ const Discord = require("discord.js")
 
 exports.run = async (client, message, args) => {
 var list = [
-  'https://cdn.discordapp.com/attachments/753628468398260265/768284648324595722/unknown.png',
-  'https://cdn.discordapp.com/attachments/753628468398260265/768284969218736168/unknown.png',
-  'https://cdn.discordapp.com/attachments/753628468398260265/768284648324595722/unknown.png',
-  'https://cdn.discordapp.com/attachments/753628468398260265/768284969218736168/unknown.png',
-  'https://cdn.discordapp.com/attachments/753628468398260265/768284648324595722/unknown.png',
-  'https://cdn.discordapp.com/attachments/753628468398260265/768284969218736168/unknown.png',
-  'https://cdn.discordapp.com/attachments/753628468398260265/768284648324595722/unknown.png',
-  'https://cdn.discordapp.com/attachments/753628468398260265/768284969218736168/unknown.png',
-  'https://cdn.discordapp.com/attachments/753628468398260265/768284648324595722/unknown.png',
-  'https://cdn.discordapp.com/attachments/753628468398260265/768284969218736168/unknown.png',
-  'https://cdn.discordapp.com/attachments/753628468398260265/768284648324595722/unknown.png',
-  'https://cdn.discordapp.com/attachments/753628468398260265/768284969218736168/unknown.png'
+  'https://im7.ezgif.com/tmp/ezgif-7-1b3d62fa3714.gif',
+  'https://im7.ezgif.com/tmp/ezgif-7-70733f4e581f.gif',
+  'https://im7.ezgif.com/tmp/ezgif-7-6e06bb500970.gif'
 ];
 var rand = list[Math.floor(Math.random() * list.length)];
 
@@ -21,7 +12,6 @@ message.channel.send({
   content: "",
   embed: {
     title: "sera que o Knuckles aprovou seu meme?",
-    description: "vamos ver >:3...",
     color: 1639005,
     image: {
       url: `` + (rand) + ``
